40b9b87440198ca0acc2b49e584d40d3fb032381
hasib095 <156058545+hasib095@users.noreply.github.com>
2024-10-26 23:47:00 +0600

    Week 1

---
+++ Week 1/Day 1/tempCodeRunnerFile.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+const int mod = 1e9 + 7;
+void file()
+{
+    #ifndef ONLINE_JUDGE 
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+}
+void solve()
+{
+    int a,b,c;cin>>a>>b>>c;
+    int mn=min(a,b);
+    int mx=max(a,b);
+    
+    for(int i=mn;i<=mx;i++)
+    {
+        if(i%c==0)
+        {
+        cout<<i<<endl;
+        break;
+        }
+
+    }
+   cout<<-1<<endl;
+}
+
+int main()
+{   file();
+    optimize();
+    solve();
+    //int t; cin >> t; while(t--){solve();}
+    return 0;
+}
